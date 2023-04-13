e879b4f58a3b665464ebc76ac70084338184445e
calebhd03 <calebhd03@gmail.com>
2023-04-13 01:29:38 -0400

    cleaned import packages

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -1,9 +1,6 @@
 using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.InputSystem.Utilities;
 
 public class Character : MonoBehaviour
 {
--- Assets/Scripts/HandgunController.cs
+++ Assets/Scripts/HandgunController.cs
@@ -1,9 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
-using UnityEngine.InputSystem.Utilities;
-using UnityEngine.UIElements;
 
 public class HandgunController : MonoBehaviour
 {
