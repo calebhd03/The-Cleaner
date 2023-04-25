c637deb4e3de089485fc0ca8d917a2ee7a528d7c
calebhd03 <calebhd03@gmail.com>
2023-04-25 02:35:40 -0400

    Fixed pulser

--- Assets/Scripts/DoorPulser.cs
+++ Assets/Scripts/DoorPulser.cs
@@ -5,17 +5,17 @@ using UnityEngine;
 public class DoorPulser : MonoBehaviour
 {
     public float doorScaleSize;
+    public float lifeTime;
+
     // Start is called before the first frame update
     void Start()
     {
-        Destroy(gameObject, 5f);
+        Destroy(gameObject, lifeTime);
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         transform.localScale = new Vector3(transform.localScale.x + doorScaleSize, 1, transform.localScale.z + doorScaleSize);
-        if(transform.localScale.x == 500f)
-            Destroy(gameObject);
     }
 }
